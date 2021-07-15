70a41c617aa7abb1cc935e11caa561dc64c5756c
aymane hahcham <aymanehachchaming@gmail.com>
2021-07-15 10:31:49 +0100

    Analytics update

---
+++ precache-manifest.343142ee66b051cfc637f7cd3d6da12c.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2caf6911944564c2cbaee58926f9cdbb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0f1593a819a0f18b2636",
+    "url": "/static/css/3.176be3fe.chunk.css"
+  },
+  {
+    "revision": "7b36f53ccc55f37e2159",
+    "url": "/static/css/4.16294f77.chunk.css"
+  },
+  {
+    "revision": "a2fcff055dc1a8f13c65",
+    "url": "/static/css/main.7114ae73.chunk.css"
+  },
+  {
+    "revision": "44ae6168f4a5f370cb62",
+    "url": "/static/js/2.521a8e39.chunk.js"
+  },
+  {
+    "revision": "f3804f06d2b907778b21d8f339f6ed0f",
+    "url": "/static/js/2.521a8e39.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f1593a819a0f18b2636",
+    "url": "/static/js/3.35ff920d.chunk.js"
+  },
+  {
+    "revision": "7b36f53ccc55f37e2159",
+    "url": "/static/js/4.17807e9b.chunk.js"
+  },
+  {
+    "revision": "a2fcff055dc1a8f13c65",
+    "url": "/static/js/main.467ace6f.chunk.js"
+  },
+  {
+    "revision": "fb3989bb03e3a52c69d9",
+    "url": "/static/js/runtime-main.53e040f4.js"
+  },
+  {
+    "revision": "5bb757ed57a7600293e04ee61d8c5db2",
+    "url": "/static/media/2project.5bb757ed.jpeg"
+  },
+  {
+    "revision": "adfd63f71325de42517467a0cca1e134",
+    "url": "/static/media/3project.adfd63f7.jpeg"
+  },
+  {
+    "revision": "6410d2801b23db7b4d465c9641ef29bd",
+    "url": "/static/media/4project.6410d280.jpg"
+  },
+  {
+    "revision": "5ffd7a7b6dfb647bd2de76c7611a3d89",
+    "url": "/static/media/Laureate.5ffd7a7b.jpeg"
+  },
+  {
+    "revision": "dba0c688b8d5ee09a1e214aebd5d25e4",
+    "url": "/static/media/Product Sans Bold.dba0c688.ttf"
+  },
+  {
+    "revision": "eae9c18cee82a8a1a52e654911f8fe83",
+    "url": "/static/media/Product Sans Regular.eae9c18c.ttf"
+  },
+  {
+    "revision": "af3440dabd89afa23c3f326dceceecc8",
+    "url": "/static/media/Yellowtail-Regular.af3440da.woff"
+  },
+  {
+    "revision": "a35177b70f29b4a567833ac255820c4e",
+    "url": "/static/media/contact.a35177b7.svg"
+  },
+  {
+    "revision": "9fdcbe4cdd92fc2e9cf1472f9a967b79",
+    "url": "/static/media/data-analyst.9fdcbe4c.svg"
+  },
+  {
+    "revision": "1c9e9aa09b31e01b109cb25c91221f21",
+    "url": "/static/media/db-logo.1c9e9aa0.jpg"
+  },
+  {
+    "revision": "829ad0a1deef4d7295adfecdf12df2be",
+    "url": "/static/media/nextuLogo.829ad0a1.webp"
+  },
+  {
+    "revision": "c1142114e3af8102f2112818961c1ee8",
+    "url": "/static/media/nvidia-logo-big.c1142114.jpg"
+  },
+  {
+    "revision": "25018d29d5ab115bc51c3e9a2d92f90f",
+    "url": "/static/media/saayaHealthLogo.25018d29.webp"
+  },
+  {
+    "revision": "834cf215a0aa78dd790f4179bc79df4d",
+    "url": "/static/media/skills.834cf215.svg"
+  },
+  {
+    "revision": "13780cdd0144cacdef25486e2c2503f8",
+    "url": "/static/media/talksCardBack.13780cdd.svg"
+  }
+]);
